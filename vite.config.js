b27c4ae5bd565076f6c9d11816ab2af236c18310
SiljeW <143178929+SiljeW@users.noreply.github.com>
2025-04-23 13:08:17 +0200

    Fix: Vite config to include css source

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,17 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          // Keep your stylesheet predictable and un-hashed
+          if (assetInfo.name === 'style.css') return 'assets/style.css';
+          return 'assets/[name]-[hash][extname]';
+        }
+      }
+    }
+  }
 })
